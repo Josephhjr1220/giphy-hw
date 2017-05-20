039cd92d7acb8936e66327662052185b0121f58d
Joseph Hernandez <josephhernandez@Josephs-MacBook-Pro.local>
2017-05-19 21:55:31 -0700

    Added HTTPS to ajax request

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -28,7 +28,7 @@ function addNewButton(){
 // Function that displays all of the gifs
 function displayGifs(){
     var animal = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
     console.log(queryURL);
     $.ajax({
         url: queryURL,
